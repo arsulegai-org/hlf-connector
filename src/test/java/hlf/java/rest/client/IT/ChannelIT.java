50d82bcbd9a98c085f5cd7aafc26a1dfb86f8821
weihong-ou <weihong.ou@walmart.com>
2022-08-09 11:12:46 -0500

    update connection file for integration test
    Signed-off-by: weihong-ou <weihong.ou@walmart.com>

--- src/test/java/hlf/java/rest/client/IT/ChannelIT.java
+++ src/test/java/hlf/java/rest/client/IT/ChannelIT.java
@@ -18,9 +18,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ActiveProfiles("intg")
 public class ChannelIT {
 
   @Autowired ChannelService channelService;
